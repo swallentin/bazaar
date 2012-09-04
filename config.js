75839fa828be25dc57fbbc70b35d2e23fa707f7f
swallentin <stephan.wallentin@gmail.com>
2012-09-04 16:34:10 +0800

    update of config

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
   , redis: process.env.REDISTOGO_URL || 'redis://local.dev/bazaar'
   , appUrl: process.env.APP_URL || 'http://local.dev:3000'
   , fb: {
-        appId: '297348623622954'
-      , appSecret: 'daa93e282fa734af9045b2ca9d0461eb'
+        appId: process.env.FB_APPID || '297348623622954'
+      , appSecret: process.env.FB_APPSECRET || 'daa93e282fa734af9045b2ca9d0461eb'
     }
   , page: {
-    project: "Blombergs Bazaar"
+    project: "Wallenbergs Bazaar"
   }
 };
 
